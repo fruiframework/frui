6a15a3e955d763131c8b23f031914c1ab3416158
iglak <iglak.me@gmail.com>
2022-11-11 12:58:46 +0100

    refactor: rename `common` to `types`

--- crates/frui_core/src/api/contexts/render_ctx/mod.rs
+++ crates/frui_core/src/api/contexts/render_ctx/mod.rs
@@ -1,13 +1,13 @@
-mod common;
 mod ext;
 mod paint_ctx;
 mod parent_data;
 mod render_ctx;
 mod render_state;
+mod types;
 
-pub use common::*;
 pub use ext::*;
 pub use paint_ctx::*;
 pub use parent_data::*;
 pub use render_ctx::*;
 pub use render_state::*;
+pub use types::*;
